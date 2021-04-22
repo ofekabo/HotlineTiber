d474fc4a5075e3a689ffeb95960607cd1d2418a4
ofekabo <76842189+ofekabo@users.noreply.github.com>
2021-04-22 15:09:44 +0300

    added healthpickup , healthui

---
+++ Assets/Scripts/Game/PlayerUIManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIManager : MonoBehaviour
+{
+   [SerializeField] Slider HealthSlider;
+   
+   [SerializeField] PlayerHealth playerHealthScript;
+   
+   private void Start()
+   {
+      HealthSlider.maxValue = playerHealthScript.maxHealth;
+      HealthSlider.value = playerHealthScript.maxHealth;
+      PlayerHealth.UpdateInterface += HandleHealthSlider;
+         
+   }
+
+   void HandleHealthSlider()
+   {
+      HealthSlider.value = playerHealthScript.currentHealth;
+   }
+
+ 
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -10,8 +10,27 @@ public class PlayerHealth : Health
     private PlayerController _pc;
     private bool _isAlive;
     public bool IsAlive { get => _isAlive; }
+
+
+    #region Events
     
-    public static event Action PlayerDied;
+    public static event Action UpdateInterface;
+ 
+
+    private void UpdateUI()
+    {
+        UpdateInterface?.Invoke();
+    }
+    
+    public static event Action<float> ReturnCurrentHealth;
+    
+    private void GetCurrentHealth()
+    {
+        ReturnCurrentHealth?.Invoke(currentHealth);
+    }
+    
+    #endregion
+    
     public override void OnStart()
     {
         _ragdollController = GetComponent<RagdollController>();
@@ -23,7 +42,6 @@ public class PlayerHealth : Health
     
     public override void OnDeath(Vector3 direction,float force)
     {
-        PlayerDied?.Invoke();
         _ragdollController.ActivateRagdoll();
         _activeWeapon.DropWeapon();
         _pc.enabled = false;
@@ -31,10 +49,14 @@ public class PlayerHealth : Health
     }
     public override void OnDamage(Vector3 direction,float force)
     {
-        
-            print(currentHealth);
-        
+       UpdateUI();
     }
-    
-    
+
+    public void AddHealth(float healthToAdd)
+    {
+        currentHealth = Mathf.Min(currentHealth += healthToAdd,100);
+        UpdateUI();
+    }
+
+  
 }
